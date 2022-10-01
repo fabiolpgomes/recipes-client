c0af52578dcacd93a036a05997768b99e6586159
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-10-01 03:20:35 +0200

    BG light

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -2,8 +2,8 @@ import SignUpForm from "../../components/SignUpForm";
 
 function SignUp() {
   return (
-    <div className="container-xl main-container bg-secondary border border-dark rounded p-3">
-      <h2>CRIAR CONTA</h2>
+    <div className="container-md main-container p-3 mb-2 bg-light text-dark border border-dark rounded p-3">
+      <h2>Create Account</h2>
       <SignUpForm />
     </div>
   );
